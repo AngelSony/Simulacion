f9a9e1d1fa7e378d8a5c06629965d36bd9b07f0d
AngelSony <angelfol@outlook.com>
2022-03-20 11:16:44 -0300

    Ordenamiento

--- Ruleta/Ruleta/Ruleta.py
+++ Ruleta/Ruleta/Ruleta.py
@@ -1,31 +1,18 @@
 import numpy.random as nr
 import matplotlib.pyplot as plt
 class Funciones:
-    def suma(self, lista):
-        sum = 0
-        for i in range (0, len(lista)):
-            sum += lista[i]
-        return sum
-
-    def graficar(self, lista):
+    def graficar(self):
         sum = 0
         ejex = []
-        ejey = []
-        for i in range (0, len(lista)):
-            sum += lista[i]
+        lista = []
+        for i in range (0, 9999):
+            sum += nr.randint(0,36)
+            print(sum)
             ejex.append(i+1)
-            ejey.append(sum/(i+1))
-        plt.plot(ejex,ejey, 'r')
+            lista.append(sum/(i+1))
+        plt.plot(ejex,lista, 'r')
         plt.show()
 
 class Ruleta:
     obj = Funciones()
-    list = []
-    sum = 0
-    for i in range (1, 9999):
-        list.append(nr.randint(0,36))
-        sum += list[i-1]
-        print(sum/i)
-    print("Resultado final: ")
-    print(obj.suma(list)/i)
-    obj.graficar(list)
+    obj.graficar()

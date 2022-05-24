683860005c402e9c54abf7bb38907acf2e2a337b
AngelSony <angelfol@outlook.com>
2022-05-23 22:52:29 -0300

    Resuleta simulación

--- RP3_1_Modelo_Eventos_Discretos.py
+++ RP3_1_Modelo_Eventos_Discretos.py
@@ -7,25 +7,26 @@ Beta = .66
 ArrivalTimes = []
 DepartureTimes = []
 EventTimes = []
-global SystemState
-SystemState = "Free"
+
 
 def Main_Program():
-    DispachedClients = 0
-    Clock = .0
-    
-    EventType = ""
-    Queue = 0
+    SystemState = ['Free']
 
-    Initialization_Routine(ArrivalTimes,DepartureTimes)
-    while DispachedClients < 10:
-        Timing_Routine(Clock,ArrivalTimes,DepartureTimes,EventType)
-        Event_Routine(EventType,Queue,DispachedClients,SystemState)
+    DispachedClients = [0]
+    Clock = [.0]
+    
+    EventType = [""]
+    Queue = [0]
+
+    Initialization_Routine()
+    while DispachedClients[0] < 10:
+        Timing_Routine(SystemState, EventType, Clock,DispachedClients)
+        Event_Routine(SystemState,EventType,Queue)
     print("EventTimes:")
     print(EventTimes)
     #Report_Generator()
 
-def Initialization_Routine(ArrivalTimes,DepartureTimes):
+def Initialization_Routine():
     for Value in UValues:
         ArrivalTimes.append(truncate(-1 * Alfa * np.log(Value),4))
         DepartureTimes.append(truncate(-1 * Beta * np.log(Value),4))
@@ -34,37 +35,35 @@ def Initialization_Routine(ArrivalTimes,DepartureTimes):
     print("DepartureTimes:")
     print(DepartureTimes)
 
-    return ArrivalTimes,DepartureTimes
-
-def Timing_Routine(Clock,ArrivalTimes,DepartureTimes,EventType):
-    if (SystemState == "Free"):
-        EventType  = "Arrival"
-        Clock += ArrivalTimes[0]
+def Timing_Routine(SystemState,EventType, Clock,DispachedClients):
+    if (SystemState[0] == "Free"):
+        EventType[0]  = "Arrival"
+        Clock[0] += ArrivalTimes[0]
         ArrivalTimes.pop(0) #Elimina el primer elemento de la lista
     else:
-        if (ArrivalTimes[0] < DepartureTimes[0]):
-            EventType  = 'Arrival'
-            Clock += ArrivalTimes[0]
+        if (len(ArrivalTimes) != 0 and ArrivalTimes[0] < DepartureTimes[0]):
+            EventType[0]  = 'Arrival'
+            Clock[0] += ArrivalTimes[0]
             ArrivalTimes.pop(0) #Elimina el primer elemento de la lista
         else:
-            EventType  = 'Departure'
-            Clock += DepartureTimes[0]
-            DepartureTimes.pop(0) #Elimina el primer elemento de la lista        
-    EventTimes.append(Clock)
-    return EventType
+            EventType[0]  = 'Departure'
+            Clock[0] += DepartureTimes[0]
+            DepartureTimes.pop(0) #Elimina el primer elemento de la lista
+            DispachedClients[0] += 1
+    EventTimes.append(truncate(Clock[0],4))
 
-def Event_Routine(EventType,Queue,DispachedClients,SystemState):
-    if (EventType == 'Arrival'):
-        if (SystemState == 'Free'):
-            SystemState = 'Busy'
+def Event_Routine(SystemState,EventType,Queue):
+    if (EventType[0] == 'Arrival'):
+        if (SystemState[0] == 'Free'):
+            SystemState[0] = 'Busy'
         else:
-            Queue += 1
-    else: # EventType = 'Partida'
-        DispachedClients += 1
-        if (Queue == 0 ):
-            SystemState = 'Free'
+            Queue[0] += 1
+    else: # EventType = 'Departure'
+       
+        if (Queue[0] == 0 ):
+            SystemState[0] = 'Free'
         else:
-            Queue -= 1
+            Queue[0] -= 1
 
 def Report_Generator():
     print("REPORTE AQUI")
